0af90252be47c7e0ba7499d19b4e9aebba478c94
Aakanksha3010 <69424461+Aakanksha3010@users.noreply.github.com>
2025-05-28 03:02:52 +0530

    Update matiks_dataanalysis.py

--- matiks_dataanalysis.py
+++ matiks_dataanalysis.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1hMzLDvR6qCcRe7jKSvTfvHKI10r8f-MC
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import plotly.express as px
